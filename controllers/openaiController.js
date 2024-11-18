4021904b9d3c71e42511bf5c580ba92b691771d8
Chrilleweb <christian.munknissen@gmail.com>
2024-11-18 17:10:10 +0100

    removed weekly reports

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,10 +1,7 @@
 import OpenAI from "openai";
 import dotenv from "dotenv";
 import Rapport from "../models/report.model.js";
-import {
-  reportGeneratorPrompt,
-  weeklyReportGeneratorPrompt,
-} from "../lib/utils/reportPromt.js";
+import { reportGeneratorPrompt } from "../lib/utils/reportPromt.js";
 
 dotenv.config();
 
@@ -47,47 +44,6 @@ export const processReports = async (req, res) => {
     const processedData = response.choices[0].message.content;
 
     // Returnér behandlet data
-    res.json({ processedData });
-  } catch (error) {
-    console.error(
-      "Error with OpenAI API:",
-      error.response?.data || error.message
-    );
-    res.status(500).json({ error: "Fejl ved behandling af data med OpenAI." });
-  }
-};
-
-export const processWeeklyReports = async (req, res) => {
-  const { reportTypeIds } = req.body;
-
-  try {
-    const reports = await Rapport.getReportsWithCommentsByTypeIds(
-      reportTypeIds,
-      7
-    );
-
-    const reportContents = reports.map((report) => ({
-      content: report.content,
-      created_at: report.created_at,
-      user: `${report.firstname} ${report.lastname}`,
-      report_type: report.report_type,
-      comments: report.comments.map((comment) => ({
-        content: comment.content,
-        created_at: comment.created_at,
-        user: `${comment.firstname} ${comment.lastname}`,
-      })),
-    }));
-
-    const response = await openai.chat.completions.create({
-      model: "gpt-4",
-      messages: [
-        { role: "system", content: weeklyReportGeneratorPrompt },
-        { role: "user", content: JSON.stringify(reportContents) },
-      ],
-    });
-
-    const processedData = response.choices[0].message.content;
-
     res.json({ processedData });
   } catch (error) {
     console.error(
--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 import { authenticateToken } from '../middleware/jwtToken.js';
-import { processReports, processWeeklyReports, processReportsWithDates } from '../controllers/openaiController.js';
+import { processReports, processReportsWithDates } from '../controllers/openaiController.js';
 
 const router = Router();
 
 router.use(authenticateToken);
 
 router.post('/process-reports', processReports);
-router.post('/process-weekly-reports', processWeeklyReports);
 router.post('/process-reports-dates', processReportsWithDates);
 
 export default router;
