b82d50a20dd99cf7a3a694592a43a13cf28e2863
Chrilleweb <christian.munknissen@gmail.com>
2024-12-25 22:17:53 +0100

    changed back to 15 min

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -2,7 +2,7 @@ import ratelimiter from "express-rate-limit";
 
 const limiter = ratelimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 2, // limit each IP to 15 requests
+  max: 15, // limit each IP to 15 requests
   handler: (req, res) => {
     console.error("Rate limit reached for /login on IP:", req.ip);
     res.status(429).json({ message: "For mange forespørgsler, prøv igen senere" });
